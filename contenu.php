b27e4152b500fbdbe150d51848db93c078e81172
ysabel06 <45562815+ysabel06@users.noreply.github.com>
2019-02-05 10:23:11 +0100

    Add files via upload

---
+++ contenu.php
@@ -0,0 +1,144 @@
+
+<div id="contenu" class="container-fluid page1">
+  <div class="row">
+    <div class="col-xs-12 col-sm-4 col-md-4 hgauche"><p class="stop">N'ATTENDEZ PLUS</p>
+      <div class="col-xs-12 col-sm-12 col-md-12 imageP"></div>
+    </div>
+    
+    <div class="col-xs-12 col-sm-4 col-md-8 welcome">
+      <p class="bienvenue">Bienvenue</p>
+      <p class="p1">Nous le savons, en qualité de dirigeant de TPE ou PME, plusieurs tâches et responsabilités vous incombent dont la gestion des ressources humaines.</p>
+      <p class="p2">Cependant, ce domaine en constante évolution peut demander énormément de temps et de recherches. Une mauvaise maîtrise peut avoir des conséquences lourdes et définitives sur la bonne marche de votre entreprise : organisation, recrutement, formation, conflits, litiges, contentieux, ...</p>
+      <p class="p3"> C'est pourquoi drh-inside.fr, est là pour vous accompagner dans vos démarches et apporter toute sa connaissance dans le management de vos ressources humaines.</p>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-3 actu">
+      <p class="act">ACTUALITES</p>
+      <div class="col-sm-12 col-md-12 t1">
+          <p class="test1"><span class="D1">26/03/2015 -test1</span>
+            <br>
+            lorem ipsumkqsdcjfklqsdjfsqdjqlskdlkqsjkqsljklqsjlkqsjlkqsdkjlqs<br>ldjqsldjlqsjdklqsjdsqdlkqsdjqslkdjqlsjdlqsjdlqs
+          </p>
+      </div>
+
+      <div class="col-xs-12 col-sm-12 col-md-12 t2">
+          <p class="test2"><span class="D2">25/03/2015 -test2</span>
+            <br>
+            lorem ipsumkqsdcjfklqsdjfsqdjqlskdlkqsjkqsljklqsjlkqsjlkqsdkjlqs<br>ldjqsldjlqsjdklqsjdsqdlkqsdjqslkdjqlsjdlqsjdlqs
+          </p>
+      </div>
+
+      <div class="col-sm-12 col-md-12 t3">
+          <p class="test3"><span class="D3">24/03/2015 -test3</span>
+            <br>
+            lorem ipsumkqsdcjfklqsdjfsqdjqlskdlkqsjkqsljklqsjlkqsjlkqsdkjlqs<br>ldjqsldjlqsjdklqsjdsqdlkqsdjqslkdjqlsjdlqsjdlqs<br>jbjkhkjnhklnkjlnklnjlk,jmlk,kl:m,k:l,:lk<br>jbjkhkjnhklnkjlnklnjlk,jmlk,kl:m,k:l,:lk
+          </p>
+      </div>
+    </div>
+
+    <div class="col-sm-12 col-md-5 recru">
+      <div class="col-sm-12 col-md-12 recrutement">
+        <figure>
+          <img src="women.png" alt="women" class="femme img-responsive">
+        </figure>
+        <p><span class="rct">Recrutement du personnel</span><br>
+        <span class="rct2">Vos recrutements d'aujourd'hui sont vos performances de demain !</span></p>
+      </div>
+
+      <div class="col-sm-12 col-md-12 recrutement">
+         <figure>
+          <img src="mail.png" alt="women" class="femme">
+        </figure>
+        <p><span class="rct">Election professionelle</span><br>
+        <span class="rct2">En tant qu'employeur, vous avez l'obligation d'organiser des elections du personnel tous les 3 mois</span></p>
+      </div>
+
+       <div class="col-sm-12 col-md-12 recrutement">
+        <figure>
+          <img src="justice2.png" alt="women" class="femme">
+        </figure>
+        <p><span class="rct">Conseil extra judiciaire</span><br>
+        <span class="rct2">DRH-INSIDE a l'avantage de disposer d'experts en droits du travail capable d'offrir des services de hautes qualités</span></p>
+      </div>
+      
+
+    </div>
+    <div class="col-md-4 gestion">
+       <div class="col-sm-12 col-md-12 recrutement">
+        <figure>
+          <img src="group.png" alt="women" class="femme">
+        </figure>
+          <p><span class="rct">Gestion du personnel</span><br>
+          <span class="rct2">Le droit social evolue et la gestion du personnel se complexifie quotidienement</span></p>
+         
+      </div>
+    <div class="col-sm-12 col-md-12 recrutement">
+        <figure>
+          <img src="poignée.png" alt="women" class="femme">
+        </figure>
+          <p><span class="rct">Relations administrative</span><br>
+          <span class="rct2">Etre confronté à une intervention de l'inspection du travail n'est pas forcément aisées et peut même parfois être source de stress</span></p>
+         
+      </div>
+  </div>
+  </div>
+</div>
+
+<div id="contenu" class="container-fluid">
+  
+<div id="contenu2" class="container-fluid">
+          <div class="page2">
+                <h3 class="textP2">Expertise</h3><br>
+                <p>Simplon, c’est une équipe d’experts : la bonne personne au bon endroit
+                 pour une performance optimale.</p>
+                 <h3 class="textP2">Expérience Utilisateur</h3><br>
+                 <p>Notre point fort : à l’écoute de l’utilisateur nous travaillons sans relâche à optimiser la solution Inside pour mieux répondre à vos attentes et enjeux. </p>
+                 <h3 class="textP2">Disponibilité et Empathie</h3><br>
+                 <p>Nous aimons notre métier et la solution que nous développons.
+                   Mais par-dessus-tout, nous aimons être utiles, et faciliter la vie de nos utilisateurs.</p>
+                   <img src="https://www.inside-reporting.fr/wp-content/uploads/2016/05/photo-societe-infineo-inside.png">
+          </div>
+</div>
+<div id="contenu3" class="container-fluid">
+          <div class="page3">
+          <h2>L'Equipe de DRH-Inside</h2>
+          <img src="img/storm.png">
+<div class="membres">
+          <h3>Membres :</h3><br>
+          <a href="https://github.com/ysabel06">Ysabelle</a>
+          <br>
+          <a href="https://github.com/Staaz1990">Alexandre</a>
+          <br>
+          <a href="https://github.com/yooo83">Yoann</a>
+          <br>
+          <a href="https://github.com/JeremyViz">Jeremy</a>
+          <br>          </div>
+<div class="simplon">
+          <h3>Groupe Simplon :</h3><br>
+          <a href="https://simplonline.co/">Simplon.com</a>
+
+</div>
+</div>
+</div>
+    <div id="formulaire" class="page4">
+        <p>Remplissez les champs ci-dessous.</p>
+        <div class="row">
+        <form action="/action_page.php">
+          <label for="fname">Prenom :</label>
+            <input type="text" id="fname" name="firstname">
+          <label for="lname">Nom :</label>
+            <input type="text" id="lname" name="lastname">
+          <label for="Email">Adresse e-mail :</label>
+            <input type="text" id="Email" name="E-mail">
+          <label for="Sujet">Sujet :</label>
+            <input type="text" id="Sujet" name="Sujet">
+          <label for="message">Message :</label>
+            <textarea id="message" name="message"></textarea> 
+          <input type="submit" value="Envoyer">
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
